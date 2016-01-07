98e7ad02812c6b90ad598fc7a00e08f1810c7b69
capooti <pcorti@gmail.com>
2016-01-07 10:51:47 -0500

    Now using latest bootstrap

--- hypermap/hypermap/settings.py
+++ hypermap/hypermap/settings.py
@@ -83,6 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # media files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
