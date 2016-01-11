403ac8e172c6e44f72d3be75f902fd02f2efcbf9
capooti <pcorti@gmail.com>
2016-01-11 16:35:27 -0500

    Added arcrest in requirements

--- hypermap/aggregator/enums.py
+++ hypermap/aggregator/enums.py
@@ -1,4 +1,6 @@
 SERVICE_TYPES = (
     ('OGC:WMS', 'Web Map Service (WMS)'),
     ('OGC:WMTS', 'Web Map Tile Service (WMTS)'),
+    ('OGC:TMS', 'Tile Map Service (TMS)'),
+    ('ESRI', 'ArcGIS REST Service'),
 )
