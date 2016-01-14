8a359a4a134fe5c0edaf89228b6e3517c5f60f72
capooti <pcorti@gmail.com>
2016-01-14 17:17:07 -0500

    Added a task to check a single service

--- hypermap/aggregator/tasks.py
+++ hypermap/aggregator/tasks.py
@@ -5,8 +5,17 @@ from models import Service
 
 @task(name="check_all_services")
 def check_all_services_task():
-    for service in Service.objects.filter(active=True):
+    for service in Service.objects.filter(active=True).order_by('-last_updated'):
         service.update_layers()
         service.check()
         for layer in service.layer_set.all():
             layer.check()
+
+
+@task(name="check_one_service")
+def check_one_service_task(service_pk):
+    service = Service.objects.get(pk=service_pk)
+    service.update_layers()
+    service.check()
+    for layer in service.layer_set.all():
+        layer.check()
