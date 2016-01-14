6bab6fe4b543c7c656d972703b199c54b0204dd1
capooti <pcorti@gmail.com>
2016-01-14 15:56:46 -0500

    Display some stats on home page

--- hypermap/aggregator/views.py
+++ hypermap/aggregator/views.py
@@ -2,15 +2,21 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 
 from models import Service, Layer
 
 
 def index(request):
-    services = Service.objects.all()
+    #services = Service.objects.all()
+    services = Service.objects.annotate(
+        num_checks=Count('resource_ptr__check')).filter(num_checks__gt=0)
+    layers_count = Layer.objects.annotate(
+        num_checks=Count('resource_ptr__check')).filter(num_checks__gt=0).count()
     template = loader.get_template('aggregator/index.html')
     context = RequestContext(request, {
         'services': services,
+        'layers_count': layers_count,
     })
     return HttpResponse(template.render(context))
 
