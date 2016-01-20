2bf6ecf336160cdd220749013d113180b9203be7
capooti <pcorti@gmail.com>
2016-01-20 16:40:28 -0500

    Moved wsgi to upper directory

--- hypermap/wsgi.py
+++ hypermap/wsgi.py
@@ -11,6 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hypermap.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.default")
 
 application = get_wsgi_application()
