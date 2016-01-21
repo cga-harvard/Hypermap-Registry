6d8551e95c610b690f708081333123629a2c7e8e
capooti <pcorti@gmail.com>
2016-01-21 17:58:30 -0500

    Now we read settings from a specific user file

--- hypermap/manage.py
+++ hypermap/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings._%s' % os.environ['USER'])
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
