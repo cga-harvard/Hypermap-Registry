8d674a22ccc22925586c594bc787c74b6820007a
capooti <pcorti@gmail.com>
2016-01-27 17:16:26 -0500

    Fixing home url

--- hypermap/hypermap/urls.py
+++ hypermap/hypermap/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    (r'^aggregator/', include('aggregator.urls')),
+    (r'^', include('aggregator.urls')),
 )
 
 if settings.DEBUG:
