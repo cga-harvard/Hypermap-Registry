8b77f94e226b1327959201528b6981488e4fe898
capooti <pcorti@gmail.com>
2016-01-27 18:08:13 -0500

    Using correct settings file

--- hypermap/hypermap/celery.py
+++ hypermap/hypermap/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypermap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.default')
 
 from django.conf import settings  # noqa
 
