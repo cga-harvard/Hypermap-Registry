c81e18299c05a1109f817d6c58a97715cc2a2932
capooti <pcorti@gmail.com>
2016-02-25 18:18:30 -0500

    Database engine for tests must be sqlite

--- hypermap/settings/default.py
+++ hypermap/settings/default.py
@@ -76,8 +76,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME', 'hypermap'),
-        'USER': os.getenv('DATABASE_USER', 'hypermap_user'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'secret'),
+        'USER': os.getenv('DATABASE_USER', 'hypermap'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'hypermap'),
         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
--- hypermap/settings/test.py
+++ hypermap/settings/test.py
@@ -3,6 +3,13 @@ from settings.default import *  # noqa
 SOLR_ENABLED = False
 SKIP_CELERY_TASK = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # BROKER_BACKEND = 'memory'
 # BROKER_URL='memory://'
 # CELERY_ALWAYS_EAGER = True
