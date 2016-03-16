7fefb99a3fd08d45b522f22b7c9550ae2200bc80
capooti <pcorti@gmail.com>
2016-03-16 13:58:17 -0400

    Using layer id in place of layer title for debug messages

--- hypermap/aggregator/solr.py
+++ hypermap/aggregator/solr.py
@@ -139,11 +139,11 @@ class SolrHypermap(object):
                                     "HalfHeight": halfHeight,
                                     "Area": area,
                                     "bbox": wkt}])
-                SolrHypermap.logger.info("Solr record saved: " + layer.title)
+                SolrHypermap.logger.info("Solr record saved for layer with id: " + layer.id)
                 return True
         except Exception:
             success = False
-            SolrHypermap.logger.error("Error svaing solr record for layer: " + layer.title)
+            SolrHypermap.logger.error("Error svaing solr record for layer with id: " + layer.id)
             return False
         print 'Layer status into solr core %s ' % (success)
 
