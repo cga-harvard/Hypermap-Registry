f4dc32fec088edddab0ff4d1abfa5b4fb55a619f
capooti <pcorti@gmail.com>
2016-03-29 19:53:17 -0400

    Added mock tests for WMS service type

---
+++ hypermap/aggregator/tests/test_wms.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the WMS Service Type.
+"""
+
+import unittest
+
+from httmock import with_httmock
+import mocks.wms
+
+from aggregator.models import Service
+
+
+class TestWorldMap(unittest.TestCase):
+
+    @with_httmock(mocks.wms.resource_get)
+    def test_create_wms_service(self):
+        # create the service
+        service = Service(
+            type='OGC_WMS',
+            url='http://wms.example.com/ows?',
+        )
+        service.save()
+
+        # check layer number
+        self.assertEqual(service.layer_set.all().count(), 9)
+
+        # check layer 0 (public)
+        layer_0 = service.layer_set.all()[0]
+        self.assertEqual(layer_0.name, 'geonode:_30river_project1_1')
+        self.assertEqual(layer_0.title, 'Rivers')
+        self.assertTrue(layer_0.is_public)
+        self.assertEqual(layer_0.keywords.all().count(), 3)
+        self.assertEqual(layer_0.srs.all().count(), 2)
+        self.assertEqual(layer_0.check_set.all().count(), 1)
+        # TODO test layer_0.layerdate_set
+
+        # test that if creating the service and is already exiting it is not being duplicated
+        # create the service
+        def create_duplicated_service():
+            duplicated_service = Service(
+                type='OGC_WMS',
+                url='http://wms.example.com/ows?',
+            )
+            duplicated_service.save()
+
+        self.assertRaises(Exception, create_duplicated_service)
+        self.assertEqual(Service.objects.all().count(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
