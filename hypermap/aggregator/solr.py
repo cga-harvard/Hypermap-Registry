5aa9e79c9807c66e354ad094fecefa748ad5db80
capooti <pcorti@gmail.com>
2016-03-31 16:59:11 -0400

    Removed unneeded code from solr

--- hypermap/aggregator/solr.py
+++ hypermap/aggregator/solr.py
@@ -2,7 +2,6 @@ import sys
 import pysolr
 import requests
 import logging
-import math
 import json
 import datetime
 
@@ -59,22 +58,6 @@ class SolrHypermap(object):
     logger = logging.getLogger("hypermap")
 
     @staticmethod
-    def good_coords(coords):
-        """ passed a string array """
-        if (len(coords) != 4):
-            return False
-        for coord in coords[0:3]:
-            try:
-                num = float(coord)
-                if (math.isnan(num)):
-                    return False
-                if (math.isinf(num)):
-                    return False
-            except ValueError:
-                return False
-        return True
-
-    @staticmethod
     def get_domain(url):
         urlParts = urlparse(url)
         hostname = urlParts.hostname
@@ -103,93 +86,79 @@ class SolrHypermap(object):
         category = None
         username = None
         try:
+            # as a first thing we need to remove the existing index in case there is already one
+            SolrHypermap.solr.delete(q='LayerId:%s' % layer.id)
+            if not layer.has_valid_bbox():
+                message = 'There are not valid coordinates for layer id: %s' % layer.id
+                SolrHypermap.logger.error(message)
+                return False, message
             bbox = [float(layer.bbox_x0), float(layer.bbox_y0), float(layer.bbox_x1), float(layer.bbox_y1)]
             for proj in layer.srs.values():
                 if proj['code'] in ('102113', '102100'):
                     bbox = mercator_to_llbbox(bbox)
-            if (SolrHypermap.good_coords(bbox)) is False:
-                print 'There are not valid coordinates for this layer ', layer.title
-                SolrHypermap.logger.error('There are not valid coordinates for layer id: %s' % layer.id)
-                return False
-            if (SolrHypermap.good_coords(bbox)):
-                minX = bbox[0]
-                minY = bbox[1]
-                maxX = bbox[2]
-                maxY = bbox[3]
-                if (minY > maxY):
-                    tmp = minY
-                    minY = maxY
-                    maxY = tmp
-                if (minX > maxX):
-                    tmp = minX
-                    minX = maxX
-                    maxX = tmp
-                centerY = (maxY + minY) / 2.0
-                centerX = (maxX + minX) / 2.0
-                halfWidth = (maxX - minX) / 2.0
-                halfHeight = (maxY - minY) / 2.0
-                area = (halfWidth * 2) * (halfHeight * 2)
-                if (minX < -180):
-                    minX = -180
-                if (maxX > 180):
-                    maxX = 180
-                if (minY < -90):
-                    minY = -90
-                if (maxY > 90):
-                    maxY = 90
-                wkt = "ENVELOPE({:f},{:f},{:f},{:f})".format(minX, maxX, maxY, minY)
-                domain = SolrHypermap.get_domain(layer.service.url)
-                if hasattr(layer, 'layerwm'):
-                    category = layer.layerwm.category
-                    username = layer.layerwm.username
-                abstract = layer.abstract
-                if abstract:
-                    abstract = strip_tags(layer.abstract)
-                else:
-                    abstract = ''
-                if layer.service.type == "WM":
-                    originator = username
-                else:
-                    originator = domain
-                # we need to remove the exising index in case there is already one
-                SolrHypermap.solr.delete(q='LayerId:%s' % layer.id)
-                # now we add the index
-                solr_record = {
-                                "LayerId": str(layer.id),
-                                "LayerName": layer.name,
-                                "LayerTitle": layer.title,
-                                "Originator": originator,
-                                "ServiceId": str(layer.service.id),
-                                "ServiceType": layer.service.type,
-                                "LayerCategory": category,
-                                "LayerUsername": username,
-                                "LayerUrl": layer.url,
-                                "LayerReliability": layer.reliability,
-                                "Is_Public": layer.is_public,
-                                "Availability": "Online",
-                                "Location": '{"layerInfoPage": "' + layer.get_absolute_url() + '"}',
-                                "Abstract": abstract,
-                                "SrsProjectionCode": layer.srs.values_list('code', flat=True),
-                                "MinY": minY,
-                                "MinX": minX,
-                                "MaxY": maxY,
-                                "MaxX": maxX,
-                                "CenterY": centerY,
-                                "CenterX": centerX,
-                                "HalfWidth": halfWidth,
-                                "HalfHeight": halfHeight,
-                                "Area": area,
-                                "bbox": wkt,
-                                "DomainName": layer.service.get_domain,
-                                }
+            minX = bbox[0]
+            minY = bbox[1]
+            maxX = bbox[2]
+            maxY = bbox[3]
+            # coords hack needed by solr
+            if (minX < -180):
+                minX = -180
+            if (maxX > 180):
+                maxX = 180
+            if (minY < -90):
+                minY = -90
+            if (maxY > 90):
+                maxY = 90
+            wkt = "ENVELOPE({:f},{:f},{:f},{:f})".format(minX, maxX, maxY, minY)
+            halfWidth = (maxX - minX) / 2.0
+            halfHeight = (maxY - minY) / 2.0
+            area = (halfWidth * 2) * (halfHeight * 2)
+            domain = SolrHypermap.get_domain(layer.service.url)
+            if hasattr(layer, 'layerwm'):
+                category = layer.layerwm.category
+                username = layer.layerwm.username
+            abstract = layer.abstract
+            if abstract:
+                abstract = strip_tags(layer.abstract)
+            else:
+                abstract = ''
+            if layer.service.type == "WM":
+                originator = username
+            else:
+                originator = domain
+            # now we add the index
+            solr_record = {
+                            "LayerId": str(layer.id),
+                            "LayerName": layer.name,
+                            "LayerTitle": layer.title,
+                            "Originator": originator,
+                            "ServiceId": str(layer.service.id),
+                            "ServiceType": layer.service.type,
+                            "LayerCategory": category,
+                            "LayerUsername": username,
+                            "LayerUrl": layer.url,
+                            "LayerReliability": layer.reliability,
+                            "Is_Public": layer.is_public,
+                            "Availability": "Online",
+                            "Location": '{"layerInfoPage": "' + layer.get_absolute_url() + '"}',
+                            "Abstract": abstract,
+                            "SrsProjectionCode": layer.srs.values_list('code', flat=True),
+                            "MinY": minY,
+                            "MinX": minX,
+                            "MaxY": maxY,
+                            "MaxX": maxX,
+                            "Area": area,
+                            "bbox": wkt,
+                            "DomainName": layer.service.get_domain,
+                            }
 
-                solr_date, type = get_date(layer)
-                if solr_date is not None:
-                    solr_record['LayerDate'] = solr_date
-                    solr_record['LayerDateType'] = type
-                SolrHypermap.solr.add([solr_record])
-                SolrHypermap.logger.info("Solr record saved for layer with id: %s" % layer.id)
-                return True, None
+            solr_date, type = get_date(layer)
+            if solr_date is not None:
+                solr_record['LayerDate'] = solr_date
+                solr_record['LayerDateType'] = type
+            SolrHypermap.solr.add([solr_record])
+            SolrHypermap.logger.info("Solr record saved for layer with id: %s" % layer.id)
+            return True, None
         except Exception:
             SolrHypermap.logger.error("Error saving solr record for layer with id: %s - %s"
                                       % (layer.id, sys.exc_info()[1]))
