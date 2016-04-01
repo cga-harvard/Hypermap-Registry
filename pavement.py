68861dd5d83c0000b1896eee2167714cd987fda8
capooti <pcorti@gmail.com>
2016-04-01 15:06:20 -0400

    Added a paver task to run integration tests

--- pavement.py
+++ pavement.py
@@ -16,4 +16,12 @@ def run_tests():
     Executes the entire test suite.
     """
     sh('python hypermap/manage.py test aggregator --settings=settings.test --failfast')
+    sh('python hypermap/manage.py test dynasty --settings=settings.test --failfast')
     sh('flake8 hypermap')
+
+@task
+def run_integration_tests():
+    """
+    Executes the entire test suite.
+    """
+    sh('python hypermap/manage.py test tests.integration --settings=settings.test --failfast')
