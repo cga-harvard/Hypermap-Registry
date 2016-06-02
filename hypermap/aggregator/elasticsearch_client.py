8304f5276856757e77d33ae4bffd513ad79acb8f
Luis Pallares <thepanchi@gmail.com>
2016-06-02 17:52:41 -0500

    fix indentation

--- hypermap/aggregator/elasticsearch_client.py
+++ hypermap/aggregator/elasticsearch_client.py
@@ -176,8 +176,8 @@ class ESHypermap(object):
         # TODO: enable auto_create_index in the ES nodes to make this implicit.
         # https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html#index-creation
         # http://support.searchly.com/customer/en/portal/questions/16312889-is-automatic-index-creation-disabled-?new=16312889
-       mapping = '''
-       {
+        mapping = '''
+        {
         "mappings":{
           %s:{
             "properties":{
@@ -189,7 +189,5 @@ class ESHypermap(object):
             }
           }
          }
-        }'''  % ESHypermap.index_name
+        }''' % ESHypermap.index_name
         ESHypermap.es.indices.create(ESHypermap.index_name, ignore=[400, 404])
-
-
