b3758918a28ed075dd11aa1e44874f695a47b23f
Jorge Martinez <jorgemg0327@gmail.com>
2016-06-13 18:46:29 -0500

    Finding packages and adding files without for loops in setup.py file

--- setup.py
+++ setup.py
@@ -1,58 +1,24 @@
 import os
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-hypermap_dir = 'hypermap'
-
-for dirpath, dirnames, filenames in os.walk(hypermap_dir):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name="HHyperMap",
-    version="0.1",
-    author="",
-    author_email="",
-    description="HHyperMap",
-    long_description=(read('README.md')),
+    name='django-hhypermap',
+    version='0.2',
+    author='',
+    author_email='',
+    url='https://github.com/cga-harvard/HHypermap',
+    download_url='https://github.com/cga-harvard/HHypermap',
+    description='Use Registry in your django projects.',
+    long_description=open(os.path.join(here, 'README.md')).read(),
+    license='See LICENSE file.',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 1 - Planning',
     ],
-    license="BSD",
-    keywords="hypermap django",
-    url='https://github.com/cga-harvard/HHyperMap',
-    packages=packages,
-    data_files=data_files,
-    zip_safe=False,
     install_requires=[
         'amqplib==1.0.2',
         'arcrest==10.3',
