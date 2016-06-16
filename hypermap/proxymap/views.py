9925a58a4110870c2a15ff3b8d293e130de4b262
Ariel Nuñez <ingenieroariel@gmail.com>
2016-06-16 12:17:11 -0500

    ArcGIS ImageServer and MapServer support

--- hypermap/proxymap/views.py
+++ hypermap/proxymap/views.py
@@ -50,21 +50,6 @@ def get_mapproxy(layer, seed=False, ignore_warnings=True, renderd=False):
     bbox_srs = 'EPSG:4326'
     grid_srs = 'EPSG:3857'
 
-    if layer.type == 'ESRI:ArcGIS:MapServer' or layer.type == 'ESRI:ArcGIS:ImageServer':
-        url = str(layer.service.url).split('?')[0] + 'WMSServer?'
-
-        # blindly replace it with /arcgis/
-        url = url.replace("/ArcGIS/rest/", "/arcgis/")
-        # same for uppercase
-        url = url.replace("/arcgis/rest/", "/arcgis/")
-        # and for old versions
-        url = url.replace("ArcX/rest/services", "arcx/services")
-        # in uppercase or lowercase
-        url = url.replace("arcx/rest/services", "arcx/services")
-
-        srs = 'EPSG:3857'
-        bbox_srs = 'EPSG:3857'
-
     if layer.type == 'WARPER':
         url = str(layer.url.replace("maps//wms", "maps/wms"))
         grid_srs = 'EPSG:900913'
@@ -87,12 +72,26 @@ def get_mapproxy(layer, seed=False, ignore_warnings=True, renderd=False):
               },
            }
 
-    if layer.type == 'ESRI:ArcGIS:MapServer':
+    if layer.type == 'ESRI:ArcGIS:MapServer' or layer.type == 'ESRI:ArcGIS:ImageServer':
+        # blindly replace it with /arcgis/
+        url = url.replace("/ArcGIS/rest/", "/arcgis/")
+        # same for uppercase
+        url = url.replace("/arcgis/rest/", "/arcgis/")
+        # and for old versions
+        url = url.replace("ArcX/rest/services", "arcx/services")
+        # in uppercase or lowercase
+        url = url.replace("arcx/rest/services", "arcx/services")
+
+        srs = 'EPSG:3857'
+        bbox_srs = 'EPSG:3857'
+
         default_source = {
-                  'type': 'tile',
-                  'url': str(layer.service.url).split('?')[0] + 'tile/%(z)s/%(y)s/%(x)s',
-                  'grid': 'default_grid',
-                  'transparent': True,
+                  'type': 'arcgis',
+                  'req': {
+                     'url': str(layer.service.url).split('?')[0],
+                     'grid': 'default_grid',
+                     'transparent': True,
+                  },
         }
 
     # A source is the WMS config
