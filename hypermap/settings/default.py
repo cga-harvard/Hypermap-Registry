4b20cbb581e38cf2ec7414538ea5b1e98446ee9b
capooti <pcorti@gmail.com>
2016-06-20 14:31:32 -0400

    Fixing some code that was causing build failure

--- hypermap/settings/default.py
+++ hypermap/settings/default.py
@@ -43,7 +43,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-SEARCH_ENABLED = str2bool(os.getenv('SEARCH_ENABLED', False))
+SEARCH_ENABLED = os.getenv(str2bool('SEARCH_ENABLED'), False)
 SEARCH_TYPE = 'solr'
 SEARCH_URL = os.getenv('SEARCH_URL', 'http://127.0.0.1:8983/solr/search')
 
@@ -197,7 +197,7 @@ LOGGING = {
     }
 
 # we need to get rid of this once we figure out how to bypass the broker in tests
-SKIP_CELERY_TASK = str2bool(os.getenv('SKIP_CELERY_TASK', 'False'))
+SKIP_CELERY_TASK = os.getenv(str2bool('SKIP_CELERY_TASK'), False)
 
 # taggit
 TAGGIT_CASE_INSENSITIVE = True
