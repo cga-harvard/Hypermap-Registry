c862150bc9eba69eb6edd88af83618faa8c33aaf
Ariel Nuñez <ingenieroariel@gmail.com>
2016-06-27 14:16:04 -0500

    /registry/

--- hypermap/urls.py
+++ hypermap/urls.py
@@ -8,11 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^layer/(?P<layer_id>\d+)/map(?P<path_info>/.*)$', layer_mapproxy, name='layer_mapproxy'),
-    url(r'^layer/(?P<layer_id>\d+)/tms/(?P<z>\d+)/(?P<y>\d+)/(?P<x>\d+).png$', layer_tms, name='layer_tms'),
-    url(r'^layer/(?P<layer_id>\d+)/map/config$', layer_tms, name='layer_tms'),
-    (r'^', include('hypermap.aggregator.urls')),
-    (r'^', include('hypermap.search.urls')),
+    url(r'^registry/layer/(?P<layer_id>\d+)/map(?P<path_info>/.*)$', layer_mapproxy, name='layer_mapproxy'),
+    url(r'^registry/layer/(?P<layer_id>\d+)/map/config$', layer_tms, name='layer_tms'),
+    (r'^registry/', include('hypermap.aggregator.urls')),
+    (r'^registry/search/', include('hypermap.search.urls')),
 )
 
 urlpatterns += patterns(
