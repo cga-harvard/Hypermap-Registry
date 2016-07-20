95b741cb32252e76de8dda0371eb00417123fddc
Jorge Martínez Gómez <jorgemg0327@gmail.com>
2016-07-20 10:19:59 -0500

    Aggregator migrations merge (#95)
    * Create Layers from single service for ESRI services
    
    * Solve migrations issue

---
+++ hypermap/aggregator/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aggregator', '0002_auto_20160719_1144'),
+        ('aggregator', '0002_endpointlist_greedy'),
+    ]
+
+    operations = [
+    ]
