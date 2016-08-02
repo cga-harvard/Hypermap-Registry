972bbb56c96a46924270dd900b12769ed0208528
Luis Pallares <thepanchi@gmail.com>
2016-08-02 18:03:39 -0500

    require catalog in endpoint list

--- hypermap/aggregator/models.py
+++ hypermap/aggregator/models.py
@@ -757,7 +757,7 @@ class EndpointList(models.Model):
     """
     upload = models.FileField(upload_to='endpoint_lists')
     greedy = models.BooleanField(default=False)
-    catalog = models.ForeignKey(Catalog, blank=True, null=True)
+    catalog = models.ForeignKey(Catalog, blank=False, null=True)
 
     def __unicode__(self):
         return self.upload.name
