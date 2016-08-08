0f2b518f3812bcf07223b19dade35346d6baf690
Ariel Nuñez <ingenieroariel@gmail.com>
2016-08-08 15:14:17 -0500

    Created merge migration

---
+++ hypermap/aggregator/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aggregator', '0002_auto_20160803_1533'),
+        ('aggregator', '0009_auto_20160802_2314'),
+    ]
+
+    operations = [
+    ]
