50b1db4a241de16b89920cf4c1abc5b7fa02728f
Tom Kralidis <tomkralidis@gmail.com>
2016-09-08 15:38:42 -0400

    update pycsw release, fix packages to be alphabetical

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'django-basic-authentication-decorator',
         'elasticsearch',
         'pika',
-        'pycsw>=2.0.0',
+        'pycsw>=2.0.1',
         'flake8',
         'httmock',
         'djmp>=0.2.9',
