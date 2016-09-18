a8219d570f4823cdc2c8ff14e97bf7d7807876d5
Ariel Nuñez <ingenieroariel@gmail.com>
2016-09-17 21:35:58 -0500

    flake8

--- hypermap/search/pycsw_plugin.py
+++ hypermap/search/pycsw_plugin.py
@@ -30,9 +30,8 @@
 
 import inspect
 
-from django.db import models
 from django.db import connection
-from django.db.models import Avg, Max, Min, Count
+from django.db.models import Max, Min, Count
 from django.conf import settings
 
 from pycsw.core import util
@@ -48,6 +47,7 @@ HYPERMAP_SERVICE_TYPES = {
     'urn:x-esri:serviceType:ArcGIS:MapServer': 'ESRI:ArcGIS:MapServer',
     'urn:x-esri:serviceType:ArcGIS:ImageServer': 'ESRI:ArcGIS:ImageServer'
 }
+
 
 class HHypermapRepository(object):
     ''' Class to interact with underlying repository '''
@@ -67,13 +67,9 @@ class HHypermapRepository(object):
             self.dbtype = 'postgresql+postgis+wkt'
 
         if self.dbtype in ['sqlite', 'sqlite3']:  # load SQLite query bindings
-            cursor = connection.cursor()
-            connection.connection.create_function(
-            'query_spatial', 4, util.query_spatial)
-            connection.connection.create_function(
-            'get_anytext', 1, util.get_anytext)
-            connection.connection.create_function(
-            'get_geometry_area', 1, util.get_geometry_area)
+            connection.connection.create_function('query_spatial', 4, util.query_spatial)
+            connection.connection.create_function('get_anytext', 1, util.get_anytext)
+            connection.connection.create_function('get_geometry_area', 1, util.get_geometry_area)
 
         # generate core queryables db and obj bindings
         self.queryables = {}
@@ -81,9 +77,9 @@ class HHypermapRepository(object):
         for tname in self.context.model['typenames']:
             for qname in self.context.model['typenames'][tname]['queryables']:
                 self.queryables[qname] = {}
+                items = self.context.model['typenames'][tname]['queryables'][qname].items()
 
-                for qkey, qvalue in \
-                self.context.model['typenames'][tname]['queryables'][qname].items():
+                for qkey, qvalue in items:
                     self.queryables[qname][qkey] = qvalue
 
         # flatten all queryables
@@ -94,8 +90,8 @@ class HHypermapRepository(object):
         self.queryables['_all'].update(self.context.md_core_model['mappings'])
 
         if 'Harvest' in self.context.model['operations'] and 'Transaction' in self.context.model['operations']:
-            self.context.model['operations']['Harvest']['parameters']['ResourceType']['values'] = HYPERMAP_SERVICE_TYPES.keys()
-            self.context.model['operations']['Transaction']['parameters']['TransactionSchemas']['values'] = HYPERMAP_SERVICE_TYPES.keys()
+            self.context.model['operations']['Harvest']['parameters']['ResourceType']['values'] = HYPERMAP_SERVICE_TYPES.keys() # noqa
+            self.context.model['operations']['Transaction']['parameters']['TransactionSchemas']['values'] = HYPERMAP_SERVICE_TYPES.keys() # noqa
 
     def dataset(self):
         ''' Stub to mock a pycsw dataset object for Transactions'''
@@ -105,26 +101,23 @@ class HHypermapRepository(object):
         ''' Query by list of identifiers '''
         return self._get_repo_filter(Layer.objects).filter(id__in=ids).all()
 
-    def query_domain(self, domain, typenames, domainquerytype='list',
-        count=False):
+    def query_domain(self, domain, typenames, domainquerytype='list', count=False):
         ''' Query by property domain values '''
 
         objects = self._get_repo_filter(Layer.objects)
 
         if domainquerytype == 'range':
-            return [tuple(objects.aggregate(
-            Min(domain), Max(domain)).values())]
+            return [tuple(objects.aggregate(Min(domain), Max(domain)).values())]
         else:
             if count:
-                return [(d[domain], d['%s__count' % domain]) \
-                for d in objects.values(domain).annotate(Count(domain))]
+                return [(d[domain], d['%s__count' % domain])
+                        for d in objects.values(domain).annotate(Count(domain))]
             else:
                 return objects.values_list(domain).distinct()
 
     def query_insert(self, direction='max'):
         ''' Query to get latest (default) or earliest update to repository '''
-        from datetime import datetime
-        if direction=='min':
+        if direction == 'min':
             return Layer.objects.aggregate(
                 Min('last_updated'))['last_updated__min'].strftime('%Y-%m-%dT%H:%M:%SZ')
         return self._get_repo_filter(Layer.objects).aggregate(
@@ -134,8 +127,7 @@ class HHypermapRepository(object):
         ''' Query by source '''
         return self._get_repo_filter(Layer.objects).filter(url=source)
 
-    def query(self, constraint, sortby=None, typenames=None,
-        maxrecords=10, startposition=0):
+    def query(self, constraint, sortby=None, typenames=None, maxrecords=10, startposition=0):
         ''' Query records from underlying repository '''
 
         # run the raw query and get total
@@ -154,14 +146,18 @@ class HHypermapRepository(object):
                 if sortby['order'] == 'DESC':
                     desc = True
                 query = query.all()
-                return [str(total), sorted(query, key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))), reverse=desc)[startposition:startposition+int(maxrecords)]]
+                return [str(total),
+                        sorted(query,
+                               key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))),
+                               reverse=desc,
+                               )[startposition:startposition+int(maxrecords)]]
             else:
                 if sortby['order'] == 'DESC':
                     pname = '-%s' % sortby['propertyname']
                 else:
                     pname = sortby['propertyname']
-                return [str(total), \
-                query.order_by(pname)[startposition:startposition+int(maxrecords)]]
+                return [str(total),
+                        query.order_by(pname)[startposition:startposition+int(maxrecords)]]
         else:  # no sort
             return [str(total), query.all()[startposition:startposition+int(maxrecords)]]
 
@@ -188,11 +184,15 @@ class HHypermapRepository(object):
         try:
             if hhclass == 'Layer':
                 # TODO: better way of figuring out duplicates
-                match = Layer.objects.filter(name=source.name, title=source.title, abstract=source.abstract, is_monitored=False)
+                match = Layer.objects.filter(name=source.name,
+                                             title=source.title,
+                                             abstract=source.abstract,
+                                             is_monitored=False)
                 matches = match.all()
                 if matches:
                     if mode == 'insert':
-                        raise RuntimeError('HHypermap error: Layer %d \'%s\' already exists' % (matches[0].id, source.title))
+                        raise RuntimeError('HHypermap error: Layer %d \'%s\' already exists' % (
+                                           matches[0].id, source.title))
                     elif mode == 'update':
                         match.update(
                             name=source.name,
@@ -212,19 +212,19 @@ class HHypermapRepository(object):
                     res = Service(type=HYPERMAP_SERVICE_TYPES[resourcetype], url=source)
 
             if self.filter is not None:
-                res.catalog = Catalog.objects.get(id=int(self.filter.split()[-1]))                    
+                res.catalog = Catalog.objects.get(id=int(self.filter.split()[-1]))
             res.save()
             if keywords:
                 for kw in keywords:
                     res.keywords.add(kw)
         except Exception as err:
-           raise RuntimeError('HHypermap error: %s' % err)
+            raise RuntimeError('HHypermap error: %s' % err)
 
         # return a list of ids that were inserted or updated
         ids = []
 
         if hhclass == 'Layer':
-            ids.append({'identifier': res.uuid, 'title': res.title })
+            ids.append({'identifier': res.uuid, 'title': res.title})
         else:
             if resourcetype == 'http://www.opengis.net/cat/csw/2.0.2':
                 for res in Endpoint.objects.filter(url=source).all():
