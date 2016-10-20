ec18f5e88f929a99a830d597cabdd898dde78a68
capooti <pcorti@gmail.com>
2016-10-20 15:50:26 -0400

    Migrations from HH v0.2 to master

--- hypermap/aggregator/migrations/0002_auto_20161020_1842.py
+++ hypermap/aggregator/migrations/0002_auto_20161020_1842.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django_extensions.db.fields
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -15,10 +16,6 @@ class Migration(migrations.Migration):
         migrations.RemoveField(
             model_name='layer',
             name='catalogs',
-        ),
-        migrations.RemoveField(
-            model_name='layer',
-            name='srs',
         ),
         migrations.AddField(
             model_name='catalog',
@@ -47,13 +44,8 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='layer',
-            name='csw_last_updated',
-            field=models.CharField(default=b'2016-08-27T03:13:11Z', max_length=32, null=True, blank=True),
-        ),
-        migrations.AddField(
-            model_name='layer',
-            name='is_monitored',
-            field=models.BooleanField(default=True),
+            name='uuid',
+            field=models.UUIDField(default=uuid.uuid4, unique=True, editable=False),
         ),
         migrations.AddField(
             model_name='service',
@@ -62,13 +54,13 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='service',
-            name='csw_last_updated',
-            field=models.CharField(default=b'2016-08-27T03:13:11Z', max_length=32, null=True, blank=True),
+            name='is_monitored',
+            field=models.BooleanField(default=True),
         ),
         migrations.AddField(
             model_name='service',
-            name='srs',
-            field=models.ManyToManyField(to='aggregator.SpatialReferenceSystem', blank=True),
+            name='uuid',
+            field=models.UUIDField(default=uuid.uuid4, unique=True, editable=False),
         ),
         migrations.AlterField(
             model_name='catalog',
@@ -82,33 +74,48 @@ class Migration(migrations.Migration):
         ),
         migrations.AlterField(
             model_name='endpoint',
-            name='endpoint_list',
-            field=models.ForeignKey(blank=True, to='aggregator.EndpointList', null=True),
-        ),
-        migrations.AlterField(
-            model_name='endpoint',
             name='url',
             field=models.URLField(max_length=255),
         ),
         migrations.AlterField(
             model_name='layer',
-            name='page_url',
-            field=models.URLField(max_length=255, null=True, blank=True),
+            name='bbox_x0',
+            field=models.DecimalField(default=-180, null=True, max_digits=19, decimal_places=10, blank=True),
         ),
         migrations.AlterField(
             model_name='layer',
-            name='service',
-            field=models.ForeignKey(blank=True, to='aggregator.Service', null=True),
+            name='bbox_x1',
+            field=models.DecimalField(default=180, null=True, max_digits=19, decimal_places=10, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='layer',
+            name='bbox_y0',
+            field=models.DecimalField(default=-90, null=True, max_digits=19, decimal_places=10, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='layer',
+            name='bbox_y1',
+            field=models.DecimalField(default=90, null=True, max_digits=19, decimal_places=10, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='layer',
+            name='csw_last_updated',
+            field=models.CharField(default=b'2016-10-20T18:42:38Z', max_length=32, null=True, blank=True),
         ),
         migrations.AlterField(
             model_name='layer',
             name='type',
-            field=models.CharField(max_length=32, choices=[(b'OGC:CSW', b'Catalogue Service for the Web (CSW)'), (b'OGC:WMS', b'Web Map Service (WMS)'), (b'OGC:WMTS', b'Web Map Tile Service (WMTS)'), (b'OSGeo:TMS', b'Tile Map Service (TMS)'), (b'ESRI:ArcGIS:MapServer', b'ArcGIS REST MapServer'), (b'ESRI:ArcGIS:ImageServer', b'ArcGIS REST ImageServer'), (b'Hypermap:WorldMap', b'Harvard WorldMap'), (b'Hypermap:WARPER', b'Mapwarper')]),
+            field=models.CharField(default=b'OGC:WMS', max_length=32, choices=[(b'OGC:CSW', b'Catalogue Service for the Web (CSW)'), (b'OGC:WMS', b'Web Map Service (WMS)'), (b'OGC:WMTS', b'Web Map Tile Service (WMTS)'), (b'OSGeo:TMS', b'Tile Map Service (TMS)'), (b'ESRI:ArcGIS:MapServer', b'ArcGIS REST MapServer'), (b'ESRI:ArcGIS:ImageServer', b'ArcGIS REST ImageServer'), (b'Hypermap:WorldMap', b'Harvard WorldMap'), (b'Hypermap:WARPER', b'Mapwarper')]),
+        ),
+        migrations.AlterField(
+            model_name='service',
+            name='csw_last_updated',
+            field=models.CharField(default=b'2016-10-20T18:42:38Z', max_length=32, null=True, blank=True),
         ),
         migrations.AlterField(
             model_name='service',
             name='type',
-            field=models.CharField(max_length=32, choices=[(b'OGC:CSW', b'Catalogue Service for the Web (CSW)'), (b'OGC:WMS', b'Web Map Service (WMS)'), (b'OGC:WMTS', b'Web Map Tile Service (WMTS)'), (b'OSGeo:TMS', b'Tile Map Service (TMS)'), (b'ESRI:ArcGIS:MapServer', b'ArcGIS REST MapServer'), (b'ESRI:ArcGIS:ImageServer', b'ArcGIS REST ImageServer'), (b'Hypermap:WorldMap', b'Harvard WorldMap'), (b'Hypermap:WARPER', b'Mapwarper')]),
+            field=models.CharField(default=b'OGC:WMS', max_length=32, choices=[(b'OGC:CSW', b'Catalogue Service for the Web (CSW)'), (b'OGC:WMS', b'Web Map Service (WMS)'), (b'OGC:WMTS', b'Web Map Tile Service (WMTS)'), (b'OSGeo:TMS', b'Tile Map Service (TMS)'), (b'ESRI:ArcGIS:MapServer', b'ArcGIS REST MapServer'), (b'ESRI:ArcGIS:ImageServer', b'ArcGIS REST ImageServer'), (b'Hypermap:WorldMap', b'Harvard WorldMap'), (b'Hypermap:WARPER', b'Mapwarper')]),
         ),
         migrations.AlterUniqueTogether(
             name='endpoint',
