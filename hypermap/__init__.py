7676276e6d15bfa9ca41a0c7debeffe6de3bf935
capooti <pcorti@gmail.com>
2016-11-03 17:55:18 -0400

    Correctly importing the celery app

--- hypermap/__init__.py
+++ hypermap/__init__.py
@@ -1,2 +1,8 @@
+from __future__ import absolute_import
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celeryapp import app as celery_app  # noqa
+
 __version__ = '0.3.11'
 __description__ = 'hhypermap'
