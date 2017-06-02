297b9aac96798c0790c6cb3bf1053da0cf8c3563
Mingda Zhang <mingda.zhang.cn@gmail.com>
2017-06-02 13:21:34 -0400

    add login link in menu and relative login page

--- hypermap/urls.py
+++ hypermap/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 from .aggregator import views
 
 admin.autodiscover()
@@ -16,6 +16,11 @@ urlpatterns = patterns(
     (r'^registry/', include('hypermap.search.urls')),
     (r'^registry/', include('hypermap.aggregator.urls')),
 
+    url(r'^login/$', auth_views.login, {'template_name': 'aggregator/login.html'}),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'aggregator/logout.html'}),
+    url(r'^password_change/$', auth_views.password_change, {'template_name': 'aggregator/password_change.html'}),
+    url(r'^password_change/done/$', auth_views.password_change_done, {'template_name': 'aggregator/password_change_done.html'}),
+    url(r'^', include('django.contrib.auth.urls')),
 )
 
 urlpatterns += patterns(
