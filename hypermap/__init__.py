6512eafc88c83f625b106ce2d5cd098285abb57c
capooti <pcorti@gmail.com>
2017-11-07 15:22:58 -0500

    Bump version 0.3.12

--- hypermap/__init__.py
+++ hypermap/__init__.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import
 # Django starts so that shared_task will use this app.
 from .celeryapp import app as celery_app  # noqa
 
-__version__ = '0.3.11'
+__version__ = '0.3.12'
 __description__ = 'hhypermap'
